d59d2ab3044ac7a10c7371bb01887ccfdc378e47
Luis Gamboa <luisgamboasierra@gmail.com>
2016-01-21 14:06:38 -0800

    add

--- item-test.php
+++ item-test.php
@@ -1,2 +1,18 @@
 <?php
 //item-test.php
+
+/*
+    This serves as a resource for 
+    our second Group project
+*/
+
+
+include 'Item.php';
+
+$items[] = new Item("Burrito","Includes awesome sauce!",7.95);
+$items[] = new Item("Tamales","Includes delicious meat!",2.43);
+$items[] = new Item("Tacos","Includes spicy sauce and cream!",3.50);
+
+echo '<pre>';
+    var_dump($items);
+echo '</pre>';
